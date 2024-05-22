5c021ec2953947aa5e0c41ab3b37f9f248fa6a6b
du-cassoulet <cassoulex@gmail.com>
2024-05-22 19:40:20 +0200

    Updated telegram.ts file

--- src/websites/telegram.ts
+++ src/websites/telegram.ts
@@ -3,9 +3,11 @@ import Website from "../structures/Website";
 export default Website.fromJSON("telegram", {
   title: "Telegram",
   type: Website.Type.SOCIAL,
-  requestUrl: "https://api.instantusername.com/c/lottiefiles/{username}",
+  requestUrl: "https://api.instantusername.com/c/telegram/{username}",
   responseUrl: "https://t.me/{username}",
-  errorType: Website.ErrorType.STATUS_CODE,
+  errorType: Website.ErrorType.RESPONSE_BODY,
+  errorBody: '"available":true',
+  headers: Website.DEFAULT_HEADERS,
   requestInterval: 500,
   usernameOptions: {
     regex: /^[a-zA-Z0-9_]{5,32}$/,
