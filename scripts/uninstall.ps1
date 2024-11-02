734a64cd5c1bb8c94cce7e53dff72f8421dfb9c3
du-cassoulet <cassoulex@gmail.com>
2024-11-02 18:46:28 +0100

    Updated uninstallation script

--- scripts/uninstall.ps1
+++ scripts/uninstall.ps1
@@ -1,31 +1,12 @@
-# Killing the process if it is running
-function Kill-Hmt {
-  try {
-    Get-Process -Name hmt | Where-Object { $_.Path -eq "$PSScriptRoot\bin\hmt.exe" } | Stop-Process -Force
-  } catch [Microsoft.PowerShell.Commands.ProcessCommandException] {
-  } catch {
-    Write-Host "There are open instances of hmt.exe that could not be automatically closed."
-    exit 1
-  }
-}
-
 # Uninstalling
 try {
-  Kill-Hmt
   Remove-Item "$PSScriptRoot\bin\hmt.exe" -Force
 } catch {
-  Kill-Hmt
-  Start-Sleep -Seconds 1
-
-  try {
-    Remove-Item "$PSScriptRoot\bin\hmt.exe" -Force
-  } catch {
-    Write-Host $_
-    Write-Host "`n`nCould not delete $PSScriptRoot\bin\hmt.exe."
-    Write-Host "Please close all instances of hmt.exe and try again."
-    
-    exit 1
-  }
+  Write-Host $_
+  Write-Host "`n`nCould not delete $PSScriptRoot\bin\hmt.exe."
+  Write-Host "Please close all instances of hmt.exe and try again."
+  
+  exit 1
 }
 
 try {
