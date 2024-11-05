9432673c1a45fab42118bf2ada9c3e5a519b4b1a
du-cassoulet <cassoulex@gmail.com>
2024-11-05 23:39:51 +0100

    Removed tests and updated editorconfig

--- src/test/math.test.ts
+++
@@ -1,8 +0,0 @@
-import { test, expect } from "bun:test";
-import roundDecimal from "../util/roundDecimal";
-
-test("math-round-decimal", () => {
-  expect(roundDecimal(Math.PI, 3)).toBe(3.142);
-
-  expect(roundDecimal(Math.E, 6)).toBe(2.718282);
-});
